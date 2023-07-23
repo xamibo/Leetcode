49bd92c10c6ce1b9a4b1cf2610b66a6dbc4099d9
xamibo <136409153+xamibo@users.noreply.github.com>
2023-07-23 20:40:23 +0530

    Update Average_Value_of_Even_Numbers_That_Are_Divisible_by_Three.java

--- Math/Easy/Average_Value_of_Even_Numbers_That_Are_Divisible_by_Three.java
+++ Math/Easy/Average_Value_of_Even_Numbers_That_Are_Divisible_by_Three.java
@@ -1,1 +1,14 @@
-
+class Solution {
+    public int averageValue(int[] nums) {
+        int sum=0;
+        int count=0;
+        for(int i=0;i<nums.length;i++){
+            if((nums[i]%6)==0){
+             sum+=nums[i];
+             count++;
+            }
+        }
+        //int c=sum/count;
+        return count == 0 ? 0 : sum/count;
+    }
+}
